cc463976542392070e25b8a3402c02bb2652dbc6
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-05-01 00:24:18 +0600

    module 1

---
+++ Module 1/dist/1.5.js
@@ -0,0 +1,8 @@
+"use strict";
+// Reference type --> object
+const user = {
+    company: "Naimur Inc.", //--> literal type, exact value , cant change it
+    firstName: "Naimur",
+    //   middleName: "Rahman",
+    lastName: "Rahman",
+};
--- Module 1/dist/index.js
+++ Module 1/dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-let course = "next level web";
-console.log(course);
+let firstName = "Naimur";
+let roll = 12345;
+let isAdmin = true;
+let x = undefined;
+let y = null;
+let d;
+d = 124;
+// Array
+let friends = ["Naimur", "Sakib", "Shakib"];
+let rollList = [1, 2, 55, 6];
+rollList.push(5);
+// tuple --> array --> order --> type of values
+let coordinates = [1, 5];
+let ageName = ["Naimur", 23, true];
